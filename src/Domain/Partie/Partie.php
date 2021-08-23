6559738310b4ac0b73901df5b21ccd62dfa1c165
N3ll3 <nelleonnet@gmail.com>
2021-08-23 15:12:24 +0200

    ...

--- src/Domain/Partie/Partie.php
+++ src/Domain/Partie/Partie.php
@@ -4,10 +4,11 @@ namespace App\Domain\Partie;
 
 
 use Broadway\EventSourcing\EventSourcedAggregateRoot as EventSourcedAggregateRoot;
-use DateTime;
-use TypePartie;
 
-use function PHPSTORM_META\type;
+use DateTimeImmutable;
+
+use App\Domain\Partie\PartieEstCreee;
+use PartieCreateur;
 
 class Partie extends EventSourcedAggregateRoot
 {
@@ -23,16 +24,58 @@ class Partie extends EventSourcedAggregateRoot
 
     private $creeeLe;
 
-    private function __construct(int $partieId, TypePartie $typePartie, int $nombreOperation, DateTime $tempsImparti, DateTime $creeeLe)
-    {
-        $this->partieId = $partieId;
-        $this->typePartie = $typePartie;
-        $this->nombreOperation = $nombreOperation;
-        $this->tempsImparti = $tempsImparti;
-        $this->creeeLe = $creeeLe;
+    private $partieCreateur;
+
+
+ 
+    public function creerPartie($unPartieId,$unPartieCreateurId, $unType, $unNombreOperation, $unTempsImparti){
+        $nouvellePartie = new Self();
+
+        //creer event
+        $nouvellePartie-> apply(new PartieEstCreee($unPartieId,$unPartieCreateurId, $unType, $unNombreOperation, $unTempsImparti, new DateTimeImmutable()));
+        
+        return $nouvellePartie;
+        
     }
 
-    
 
 
+    public function applyPartieEstCreee(PartieEstCreee $event)
+    {
+        $this->partieId = $event->partieId;
+
+        // We create the entity in our event handler so that it will be createvent       // when the aggregate root is reconstituted from an event stream. Once
+        // the child entity is instantiated and returned by getChildEntities()
+        // it can emit and apply events itself.
+        $this->partieCreateur = new PartieCreateur(
+            $event->partieId,
+            $event->partieCreateurId,
+            $event->nombreOperation,
+            $event->tempsImparti,
+            $event->creeeLe,
+            $event->partieId
+        );
+    }
+
+
+    public function getAggregateRootId()
+    {
+        return $this->partieId;
+    }
+
+    public function typePartie(){
+        return $this->typePartie;
+    }
+
+    public function nombreOperation(){
+        return $this->nombreOperation;
+    }
+
+    public function tempsImparti(){
+        return $this->tempsImparti;
+    }
+
+    public function creeeLe(){
+        return $this->creeeLe;
+    }
 }
